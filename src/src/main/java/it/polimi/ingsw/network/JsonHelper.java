42cb6ade4acdb154bf869e479e36f9fb2f2d3620
TommasoLunardon <61751013+TommasoLunardon@users.noreply.github.com>
2020-04-29 13:04:16 +0200

    Add files via upload
    Old version restoring

---
+++ src/src/main/java/it/polimi/ingsw/network/JsonHelper.java
@@ -0,0 +1,53 @@
+package it.polimi.ingsw.network;
+
+import com.google.gson.*;
+import it.polimi.ingsw.network.events.Event;
+
+
+/*
+    Class that provides methods to serialize and deserialize events sent and received throughout our MVC architecture
+ */
+
+
+public class JsonHelper {
+
+    public JsonHelper(){};
+
+    public String serialization(Event event){
+
+        if(event == null){
+            throw new NullPointerException();}
+
+        GsonBuilder builder = new GsonBuilder().enableComplexMapKeySerialization();
+        Gson gson = builder.create();
+        JsonElement jsonEl = gson.toJsonTree(event);
+        jsonEl.getAsJsonObject().addProperty("category", event.getClass().toString().replace("class ", ""));;
+        return gson.toJson(jsonEl);
+
+    }
+
+
+
+    public Object deserialization(String input){
+        if(input == null){
+            throw new NullPointerException();
+        }
+        GsonBuilder builder = new GsonBuilder();
+        builder.setPrettyPrinting();
+        Gson gson = builder.create();
+        JsonParser parser = new JsonParser();
+        JsonObject obj = parser.parse(input).getAsJsonObject();
+        String category = obj.get("category").getAsString();
+        obj.remove("category");
+        Class<?> classType = null;
+        try {
+            classType = Class.forName(category);
+        } catch (ClassNotFoundException e) {
+            System.out.println();
+        }
+        return classType.cast(gson.fromJson(input, classType));
+
+
+    }
+
+}
