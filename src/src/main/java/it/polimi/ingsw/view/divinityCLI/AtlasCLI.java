391b631acb9b4d712a82f2f76298aadcc8810f1b
GabrieleGatti866714 <61782375+GabrieleGatti866714@users.noreply.github.com>
2020-05-03 10:24:44 +0200

    Add files via upload
    upload missing file view

---
+++ src/src/main/java/it/polimi/ingsw/view/divinityCLI/AtlasCLI.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.view.divinityCLI;
+
+import it.polimi.ingsw.view.divinityCLI.DivinityCLI;
+
+/**
+ * create an Atlas instance for out stamp
+ * @author Gabriele Gatti
+ */
+public class AtlasCLI extends DivinityCLI {
+    public AtlasCLI(){
+        super("Atlas","Your worker can build a dome in every level,\n ground too");
+    }
+}
---
+++ src/src/main/java/it/polimi/ingsw/view/divinityCLI/MinotaurCLI.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.view.divinityCLI;
+
+import it.polimi.ingsw.view.divinityCLI.DivinityCLI;
+
+/**
+ * create a Minotaur instance for out stamp
+ * @author Gabriele Gatti
+ */
+public class MinotaurCLI extends DivinityCLI {
+    public MinotaurCLI(){
+        super("Asterios","Your worker can move in a opponent's worker box\n if only the next box in same direction is free, the opponent's worker have to move in that box (don't care about the level)");
+    }
+}
